b4a8f3b0349c6bb924fcac053d5523590506da3b
anotherNick <nick.steimling@gmail.com>
2026-06-03 21:58:01 -0500

    Configure webpack
    Included basic dev dependencies and a module with DateTime functions.
    Began template.html, styles.css, and index.js as the basis for
    our app.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+ToDo Item, ToDo List, ToDo Container
+
+ToDo Item
+-ID
+-Due Date
+-Title
+-Description
+-Priority
+-Notes
+-Checkbox
+-Order?
+-Nesting?
+
+ToDo List
+-ID
+-Title
+-Description
+-ToDo Items
+-Order
+
+
